86117d21b314fcea08e2e4d1abf31060b8363dcb
bibitete <cjycocoho@naver.com>
2023-11-21 22:18:35 +0900

    멤버 컨트롤러 작업 이메일 api 작업 루트 컨텍스트 수정 등

--- RoomMaker/src/main/java/com/rm/controller/MemberController.java
+++ RoomMaker/src/main/java/com/rm/controller/MemberController.java
@@ -1,14 +1,20 @@
 package com.rm.controller;
 
+import java.util.Random;
+
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.rm.model.MemberVO;
@@ -22,8 +28,15 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 	
+	@Autowired
+	private JavaMailSender mailSender;
+	
 	//회원가입 페이지 이동
+<<<<<<< Updated upstream
 	@RequestMapping(value = "/join", method=RequestMethod.GET)
+=======
+	@RequestMapping(value = "join", method=RequestMethod.GET)
+>>>>>>> Stashed changes
 	public void joinGET() {
 		
 		logger.info("회원가입 페이지 진입");
@@ -72,6 +85,51 @@ public class MemberController {
 	        session.setAttribute("member", memberVO);             // 일치하는 아이디, 비밀번호 경우 (로그인 성공)
 	        
 	        return "redirect:/main";
+	    }
+	    
+	    /* 이메일 인증 */
+	    @RequestMapping(value="/mailCheck", method=RequestMethod.GET)
+	    @ResponseBody
+	    public String mailCheckGET(String email) throws Exception{
+	    	
+	    	/* 뷰(View)로부터 넘어온 데이터 확인 */
+	    	logger.info("이메일 데이터 전송 확인");
+	    	logger.info("인증번호 : " + email);
+	    	
+	    	/* 인증번호(난수) 생성 */
+	    	Random random = new Random();
+	    	int checkNum = random.nextInt(888888) + 111111;
+	    	logger.info("인증번호 " + checkNum);
+	    	
+	    	/* 이메일 보내기 */
+	    	String setFrom = "cjycocoho@naver.com"; // root-context.xml에 기입한 본인의 이메일 계정
+	    	String toMail = email;	// 수신받을 이메일입니다.뷰로부터 받은 이메일 주소인 변수 email을 사용.
+	    	String title = "회원가입 인증 이메일 입니다.";	// 자신이 보낼 이메일 제목을 작성
+	    	String content = 	// 자신이 보낼 이메일 내용
+	    			"홈페이지를 방문해주셔서 감사합니다." +
+	    			"<br><br>" +
+	    			"인증 번호는 " + checkNum + "입니다." + 
+	    			"<br>" +
+	    			"해당 인증번호를 인증번호 확인란에 기입하여 주세요.";
+	    	
+	    	try {
+	    		
+	    		MimeMessage message = mailSender.createMimeMessage();
+	    		MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+	    		helper.setFrom(setFrom);
+	    		helper.setTo(toMail);
+	    		helper.setSubject(title);
+	    		helper.setText(content,true);
+	    		mailSender.send(message);
+	    		
+	    	}catch(Exception e) {
+	    		e.printStackTrace();
+	    	}
+	    	
+	    	String num = Integer.toString(checkNum);
+	    	
+	    	return num;
+	    	
 	    }
 
 	
--- RoomMaker/src/test/java/com/rm/controller/MemberMapperTests.java
+++ RoomMaker/src/test/java/com/rm/controller/MemberMapperTests.java
@@ -1,4 +1,4 @@
-package com.rm.mapper;
+package com.rm.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.rm.mapper.MemberMapper;
 import com.rm.model.MemberVO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
